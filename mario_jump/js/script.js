d4bb8a6341e579ec7db2a4237ef62d9d28b44746
Diogo S. Florêncio <diogosflorencio@gmail.com>
2023-04-06 18:45:44 -0300

    html

---
+++ mario_jump/js/script.js
@@ -0,0 +1,32 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+function jump_mario() {
+    mario.classList.remove('bottom');
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+        mario.classList.add('bottom');
+    }, 500);
+}
+
+const loop = setInterval(() =>{
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+    if (pipePosition <= 90 && marioPosition < 55 && pipePosition > 0){
+
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens/mario_over.png'
+        mario.style.width = '60px'
+        mario.style.marginLeft = '30px'
+        clearInterval(loop);
+    }
+}, 10);
+
+document.addEventListener('keydown',jump_mario);
